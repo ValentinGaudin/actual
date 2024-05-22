dcc61f9e22c933a010c20466281ce950fb5358b5
Valentin <valentingaudin@gmail.com>
2024-05-22 09:03:12 +0200

    feat(app) candidate table

---
+++ platform/database/migrations/2024_05_21_194416_create_candidates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('email')->unique()->index();
+            $table->dateTime('birthday');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
