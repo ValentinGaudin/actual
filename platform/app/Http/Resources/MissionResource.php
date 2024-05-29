f476d7a2854c4b7de374e25f83e8bae6c6033ac1
Valentin <valentingaudin@gmail.com>
2024-05-29 23:25:12 +0200

    feat(app): remove import

--- platform/app/Http/Resources/MissionResource.php
+++ platform/app/Http/Resources/MissionResource.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Resources;
 
 use App\Models\Mission;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
